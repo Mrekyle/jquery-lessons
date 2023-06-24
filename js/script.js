26fe919e9246244bc6ddde064a0a3c09d9555935
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-06-24 18:10:23 +0100

    Add events notes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-// $(document).ready(function(){ // Ensures the page is fully loaded by the engine before running any code inside of the block
-// 	$('h2').css('text-decoration', 'underline') // Underlines any h2 elements on the page with the selector and css syntax
-// 	$('ul').css('border', 'solid 1px #ccc') // Adds a border to the ul elements that is solid and 1px wide.
-// });
+$(document).ready(function(){ // Ensures the page is fully loaded by the engine before running any code inside of the block
+	$('h2').css('text-decoration', 'underline') // Underlines any h2 elements on the page with the selector and css syntax
+	$('ul').css('border', 'solid 1px #ccc') // Adds a border to the ul elements that is solid and 1px wide.
+});
 
-// $(document).ready(function() {
-// 	$('h2').addClass('underline'); // Adding the css class to the html element instead of directly adding the styling
-// 	$('ul').addClass('border');
-// })
+$(document).ready(function() {
+	$('h2').addClass('underline'); // Adding the css class to the html element instead of directly adding the styling
+	$('ul').addClass('border');
+})
 
 /**
  * To select something in jQuery using the '$' symbol is shot hand for 'jquery'
@@ -21,3 +21,42 @@
 // .css allows us to change the css styling of the page 
 // .addClass add a class to the html element
 // .removeClass removes a class from the html element
+// :odd selects the odd numbered items
+// :even selects the even numbered items
+// .on is the same as the event listener methods in normal js
+// .mouseEnter runs the code when the mouse hovers over the element
+// .mouseLeave runs the code when the mouse leaves the element
+// .click runs the code when the selected element has had a mouse click
+
+
+$(document).ready(function() {
+    $('tr:odd').addClass('odd'); // Finds the odd number/index and applies what ever code is stated
+    $('tr:even').addClass('even') // Finds the even number/index and applies what ever code is stated
+})
+
+$(document).ready(function() {
+    $("#stream1_btn").on("click", function() { // Event handler .on and click is the same as the event listener method
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream1").addClass('highlight_stream');
+   });
+   $("#stream2_btn").click(function() { // Produces the same result as the other code above. Just cleaner
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream2").addClass('highlight_stream');
+   });
+   $("#stream3_btn").mouseOver(function() { // Runs on mouse over of the element
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream3").addClass('highlight_stream');
+   });
+   $("#stream4_btn").mouseLeave(function() { // Runs on mouse leaving the selected element 
+	$(".stream1").removeClass('highlight_stream');
+	$(".stream2").removeClass('highlight_stream');
+	$(".stream3").removeClass('highlight_stream');
+	$(".stream3").addClass('highlight_stream');
+});
+}); 
