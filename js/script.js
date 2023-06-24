748964084eef46565bc6aff86d6e7d2130f9084e
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-06-24 17:42:54 +0100

    Add jquery notes

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
-$(document).ready(function() {
- 
+// $(document).ready(function(){ // Ensures the page is fully loaded by the engine before running any code inside of the block
+// 	$('h2').css('text-decoration', 'underline') // Underlines any h2 elements on the page with the selector and css syntax
+// 	$('ul').css('border', 'solid 1px #ccc') // Adds a border to the ul elements that is solid and 1px wide.
+// });
 
- 	$("#stream1_btn").on("click", function() {
- 		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream1").addClass('highlight_stream');
-	});
-	$("#stream2_btn").on("click", function() {
-		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream2").addClass('highlight_stream');
-	});
-	$("#stream3_btn").on("click", function() {
-		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream3").addClass('highlight_stream');
-	});
+// $(document).ready(function() {
+// 	$('h2').addClass('underline'); // Adding the css class to the html element instead of directly adding the styling
+// 	$('ul').addClass('border');
+// })
 
+/**
+ * To select something in jQuery using the '$' symbol is shot hand for 'jquery'
+ * 
+ * Using the .css method changes the html document not the css document. It does it by using inline css styling on the html code
+ * We are able to get around this by creating the classes in the css file itself and using jquery to add the class where desired
+ */
 
-}); 
+// $('h2').text('Hello World') // Changes the text of the h2 elements to 'Hello World'
+// $('#myName') // Selects the 'myName' ID
+// .html allows us to change the html of the page
+// .css allows us to change the css styling of the page 
+// .addClass add a class to the html element
+// .removeClass removes a class from the html element
